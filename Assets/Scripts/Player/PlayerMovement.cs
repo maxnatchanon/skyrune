52b08f2eede63557deaa4dacf701476807bc2450
Natchanon <natchanon.347@gmail.com>
2020-03-29 02:31:01 +0700

    Add directional idle animations

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,29 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
 	public float moveSpeed = 5f;
 
 	public Rigidbody2D rb;
 	public Animator animator;
+    public Camera cam;
 
 	Vector2 movement;
 
-    void Update()
-    {
+    void Update() {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+        if (movement.x != 0 || movement.y != 0) {
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+        }
+
+        // Use for attacking?
+        // Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        // Vector2 lookDir = (mousePos - rb.position).normalized;
     }
 
-    void FixedUpdate()
-    {
-        if (movement.x != 0 && movement.y != 0)
-        {
+    void FixedUpdate() {
+        if (movement.x != 0 && movement.y != 0) {
             movement.x *= 0.7f;
             movement.y *= 0.7f;
         }
