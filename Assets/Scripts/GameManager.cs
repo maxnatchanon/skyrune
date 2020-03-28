bed9c7ff519c3208406f495bad49733f1209f73d
Natchanon <natchanon.347@gmail.com>
2020-03-29 00:47:33 +0700

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Power {
+	Fireball, Dash, Freeze
+}
+
+public class GameManager : MonoBehaviour {
+	static GameManager instance;
+
+	public int numberOfRooms;
+	public bool[] hasClearedRoom;
+	public Dictionary<Power, bool> hasUnlockedPower;
+
+	void Awake() {
+		if (instance != null) {
+			Destroy(gameObject);
+		} else {
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+			InitializeGame();
+		}
+	}
+
+	void InitializeGame() {
+		numberOfRooms = 3;
+		hasClearedRoom = new bool[] {false, false, false};
+
+		hasUnlockedPower = new Dictionary<Power, bool>();
+		foreach (Power power in (Power[]) System.Enum.GetValues(typeof(Power))) {
+			hasUnlockedPower[power] = false;
+		}
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float moveSpeed = 5f;
+
+	public Rigidbody2D rb;
+	public Animator animator;
+
+	Vector2 movement;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        if (movement.x != 0 && movement.y != 0)
+        {
+            movement.x *= 0.7f;
+            movement.y *= 0.7f;
+        }
+
+    	rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
