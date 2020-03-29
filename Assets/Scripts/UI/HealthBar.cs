eadee8d06ba09d78206064937ad45acb86f78848
Natchanon <natchanon.347@gmail.com>
2020-03-30 01:17:36 +0700

    Add UI script

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+	public RectTransform bar;
+
+    void Update() {
+    	// TODO: Poll health percentage
+    	float health = 0.2f;
+        bar.transform.localScale = new Vector3(health, 1f, 1f);
+    }
+}
---
+++ Assets/Scripts/UI/Potion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Potion : MonoBehaviour {
+	public Text numberText;
+
+    void Update() {
+        // TODO: Poll number of potions
+        int number = 43;
+        numberText.text = number.ToString();
+    }
+}
