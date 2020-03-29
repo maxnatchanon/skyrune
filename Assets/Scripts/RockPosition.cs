4ff0b252d5325623c4d1b8d88e585f1960f47774
Natchanon <natchanon.347@gmail.com>
2020-03-29 18:41:21 +0700

    Add rock

---
+++ Assets/Scripts/RockPosition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockPosition : MonoBehaviour {
+	public Transform player;
+
+    void Update() {
+    	Vector3 tmp = transform.position;
+    	float rockBottomY = transform.position.y - GetComponent<Renderer>().bounds.size.y / 2;
+		tmp.z = (player.transform.position.y <= rockBottomY + 1) ? 1 : -1;
+    	transform.position = tmp;
+    }
+}
