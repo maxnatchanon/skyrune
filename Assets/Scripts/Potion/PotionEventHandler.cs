5077069cae8ade143703911ba5460bc995fe6cc6
Natchanon <natchanon.347@gmail.com>
2020-03-30 22:57:33 +0700

    Add potion event handler

---
+++ Assets/Scripts/Potion/PotionEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionEventHandler : MonoBehaviour {
+	Transform player;
+	PlayerHoverText playerText;
+
+	void Start() {
+		player = GameObject.Find("Player").GetComponent<Transform>();
+		playerText = GameObject.Find("Player").GetComponent<PlayerHoverText>();
+	}
+
+    void Update() {
+    	Vector3 tmp = transform.position;
+    	float potionBottomY = transform.position.y - GetComponent<Renderer>().bounds.size.y / 2;
+    	tmp.z = (player.transform.position.y <= potionBottomY + 0.7) ? 1 : -1;
+    	transform.position = tmp;
+
+    	float distance = Vector3.Distance(transform.position, player.transform.position);
+    	if (distance <= 1.5f) {
+    		playerText.SetText("Potion\n(Press C to pick up)", 0.1f);
+    		if (Input.GetKeyDown("c")) {
+    			// TODO: Pick up potion
+    		}
+    	}
+    }
+}
