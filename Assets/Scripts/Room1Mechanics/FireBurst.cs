045cdd4de94833805c0bcb01ecea1024d78d8577
Natchanon <natchanon.347@gmail.com>
2020-04-11 22:29:45 +0700

    Update room 1

---
+++ Assets/Scripts/Room1Mechanics/FireBurst.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBurst : MonoBehaviour
+{
+
+    public Transform FirePos1;
+    public Transform FirePos2;
+    public GameObject Flame1;
+    public GameObject Flame2;
+    public Animator animator;
+    
+    //public float arrowForce = 20f;
+    //public float delayTime = 1f;
+    //public float shootInterval;
+    private int currentCD = 200;
+
+	//float currentDelayTime = 0f;
+
+    void Start()
+    {
+        //animator.SetFloat("Interval",shootInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentCD > 0){
+	currentCD--;}
+	else{launchFire(); currentCD = 500;}
+    }
+
+    void launchFire()
+    {
+        GameObject fire1 = Instantiate(Flame1, FirePos1.position, FirePos1.rotation);
+        Rigidbody2D rb = fire1.GetComponent<Rigidbody2D>();
+	GameObject fire2 = Instantiate(Flame2, FirePos2.position, FirePos2.rotation);
+        Rigidbody2D rb2 = fire2.GetComponent<Rigidbody2D>();
+        //rb.AddForce(FirePos1.up * -1 * arrowForce, ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/Scripts/Room1Mechanics/Flames.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flames : MonoBehaviour
+{
+	private int ticks = 0;
+	private SpriteRenderer spr;
+	private Sprite [] sprites;
+    void Start(){
+	spr = GetComponent<SpriteRenderer> ();
+	sprites = Resources.LoadAll<Sprite> ("WallOfFlame1");
+	}
+
+    void OnCollisionEnter2D(Collision2D collision){
+        //print(collision.gameObject.name);
+        Destroy(gameObject);
+    }
+
+    void update(){
+
+        ticks++;
+	if (ticks < 10){spr.sprite = sprites[1];}
+	
+	else if (ticks < 50){spr.sprite = sprites[2];}
+	
+	else if (ticks < 100){spr.sprite = sprites[3];}
+	
+	else if (ticks > 200){Destroy(gameObject);}
+	}
+
+}
