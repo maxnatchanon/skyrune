8a04064aab5326de70fd1e6a7df05789956f2d1c
sunsingsong <sunspiningsong@gmail.com>
2020-04-19 18:23:40 +0700

    Edit trap

--- Assets/Scripts/Monster/MiniBoss_2.cs
+++ Assets/Scripts/Monster/MiniBoss_2.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class MiniBoss_2 : MonoBehaviour
 {
+    private GameObject RunePickup;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        RunePickup = GameObject.Find("/YellowRunePickup");
+        RunePickup.GetComponent<RunePickup>().Unlock();
     }
 
     // Update is called once per frame
