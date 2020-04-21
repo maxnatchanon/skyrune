7de32c337395622e12c27b28b7f6079e05e5c2cc
sunsingsong <sunspiningsong@gmail.com>
2020-04-21 19:46:48 +0700

    Add scene2 button

---
+++ Assets/Scripts/Scene2/BossHealthBar_2.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHealthBar_2 : MonoBehaviour
+{
+    public RectTransform bar;
+    public GameObject boss;
+
+    void Update()
+    {
+    	float health = (float)5 / (float)10;
+        bar.transform.localScale = new Vector3(health, 1f, 1f);
+    }
+}
---
+++ Assets/Scripts/Scene2/Scene2_Manager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene2_Manager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject boss;
+    public GameObject hpBar;
+    public RectTransform bar;
+    public GameObject bunker;
+
+    public Animator Button_R;
+    public Animator Button_P;
+    public Animator Button_B;
+    public Animator Button_G;
+
+    public GameObject buttonR;
+    public GameObject buttonP;
+    public GameObject buttonB;
+    public GameObject buttonG;
+
+    private float bossMaxHp = 100;
+    private float bossHp = 100;
+    private GameObject RunePickUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boss.SetActive(false);
+        hpBar.SetActive(false);
+        bunker.SetActive(false);
+        RunePickUp = GameObject.Find("/YellowRunePickup");
+        RunePickUp.GetComponent<RunePickup>().Unlock();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!boss.active &&  GameManager.instance.pickedRune == Rune.Yellow && player.transform.position.x > -15.0f){
+            boss.SetActive(true);
+            hpBar.SetActive(true);
+            bunker.SetActive(true);
+        }
+        if (boss.active){
+            float health = (float)bossHp / (float)bossMaxHp;
+            bar.transform.localScale = new Vector3(health, 1f, 1f);
+        }
+        if (buttonR.GetComponent<Renderer>().bounds.Intersects(player.GetComponent<Renderer>().bounds)){
+            Button_R.SetBool("R_trigger",true);
+        }
+        if (buttonP.GetComponent<Renderer>().bounds.Intersects(player.GetComponent<Renderer>().bounds)){
+            Button_P.SetBool("P_trigger",true);
+        }
+        if (buttonB.GetComponent<Renderer>().bounds.Intersects(player.GetComponent<Renderer>().bounds)){
+            Button_B.SetBool("B_trigger",true);
+        }
+        if (buttonG.GetComponent<Renderer>().bounds.Intersects(player.GetComponent<Renderer>().bounds)){
+            Button_G.SetBool("G_trigger",true);
+        }
+    }
+}
