c9d8f650c7eee872e5592b92f6be649d510a3a5c
Natchanon <natchanon.347@gmail.com>
2020-04-25 20:35:23 +0700

    implement freeze skill

--- Assets/Scripts/UI/SkillUIController.cs
+++ Assets/Scripts/UI/SkillUIController.cs
@@ -11,16 +11,23 @@ public class SkillUIController : MonoBehaviour {
 
 	Color32 unlockedColor = new Color32(255, 255, 255, 255);
 	Color32 notUnlockedColor = new Color32(255, 255, 255, 0);
+    Color32 coolDownColor = new Color32(100, 100, 100, 255);
+
+    PlayerMovement player;
+
+    void Start() {
+        player = GameObject.Find("Player").GetComponent<PlayerMovement>();
+    }
 
     void Update() {
         if (skill == Skill.Freeze) {
-            img.color = GameManager.instance.hasUnlockedPower[Power.Freeze] ? unlockedColor : notUnlockedColor;
+            img.color = GameManager.instance.hasUnlockedPower[Power.Freeze] ? (player.isFreezeCoolDown ? coolDownColor : unlockedColor) : notUnlockedColor;
             txt.gameObject.SetActive(GameManager.instance.hasUnlockedPower[Power.Freeze]);
             
             float bottomPadding = GameManager.instance.hasUnlockedPower[Power.Fireball] ? 120 : 20;
             rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, bottomPadding, rt.sizeDelta.y);
         } else if (skill == Skill.Shield) {
-            img.color = GameManager.instance.hasUnlockedPower[Power.Shield] ? unlockedColor : notUnlockedColor;
+            img.color = GameManager.instance.hasUnlockedPower[Power.Shield] ? (player.isShieldCoolDown ? coolDownColor : unlockedColor) : notUnlockedColor;
             txt.gameObject.SetActive(GameManager.instance.hasUnlockedPower[Power.Shield]);
 
             float bottomPadding = GameManager.instance.hasUnlockedPower[Power.Fireball] ? 120 : 20;
