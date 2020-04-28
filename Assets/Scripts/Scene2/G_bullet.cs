3c76ebcc63916a9886ee4983000388056c43e56b
sunsingsong <sunspiningsong@gmail.com>
2020-04-28 13:55:06 +0700

    Add Room2RPBG

---
+++ Assets/Scripts/Scene2/G_bullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G_bullet : MonoBehaviour
+{
+    private GameObject scenemanager;
+    private GameObject player;
+
+    private float damageInterval=0.05f;
+    private float currentDamageTime=0;
+    private float moveSpeed = 20f;
+    private Vector3 target;
+    private Vector3 start;
+    private float startTime=0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scenemanager = GameObject.Find("/Scene2_Manager");
+        player = GameObject.Find("/Player");
+        target = player.transform.position;
+        start = transform.position;
+        target.z=0;
+        start.z=0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + (((target-start).normalized*moveSpeed) * Time.deltaTime);
+        if(gameObject.GetComponent<Renderer>().bounds.Intersects(player.GetComponent<Renderer>().bounds) && currentDamageTime>=damageInterval && !scenemanager.GetComponent<Scene2_Manager>().s4.activeSelf){
+            GameManager.instance.ReduceHealth(1);
+            currentDamageTime = 0f;
+        }
+        currentDamageTime+=Time.deltaTime;
+        startTime+=Time.deltaTime;
+        if (startTime>=5){
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Scene2/Green.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Green : MonoBehaviour
+{
+    public GameObject g_BulletPrefab;
+
+    private GameObject scenemanager;
+    private GameObject player;
+    private GameObject bullet;
+    private float angle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scenemanager = GameObject.Find("/Scene2_Manager");
+        player = GameObject.Find("/Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = player.transform.position - transform.position;
+        angle = Mathf.Atan2(dir.y,dir.x) * Mathf.Rad2Deg;
+    }
+
+    public void ShootGreen(){
+        bullet = Instantiate(g_BulletPrefab, new Vector3(transform.position.x,transform.position.y,0.1f), Quaternion.Euler (0f, 0f, angle-90f));
+    }
+}
